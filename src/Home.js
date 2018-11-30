b15590c558f8cbf337585a4e97a74691b826f0e4
oliverjam <oliverjamesphillips@gmail.com>
2018-11-30 09:09:37 +0000

    Handle missing/incomplete GH Pages site

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,18 @@ const FreeCodeCamp = lazy(() => import('./FreeCodeCamp'));
 
 function Home() {
   const { github } = useContext(UserContext);
-  const websiteUrl = github.pages[0];
+  const initialUrl = github.pages[0];
+  const [websiteUrl, setWebsiteUrl] = useState(initialUrl);
   const { progress, data } = useData(
     `/.netlify/functions/website?site=${websiteUrl}`
   );
+
+  const { codewars, freecodecamp } = data;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setWebsiteUrl(event.target.website.value);
+  }
 
   return (
     <>
@@ -18,20 +26,53 @@ function Home() {
       <section>
         <h2>1. Website</h2>
         {progress === 'success' ? (
-          <p>{websiteUrl} passed</p>
+          <>
+            {codewars && freecodecamp ? (
+              <p>{websiteUrl} passed</p>
+            ) : (
+              <p>
+                Please check your site has links to your Codewars and
+                FreeCodeCamp profiles
+              </p>
+            )}
+            <p>
+              {codewars
+                ? `Found Codewars user: ${codewars.username}`
+                : `Missing Codewars profile`}
+            </p>
+            <p>
+              {freecodecamp
+                ? `Found FreeCodecamp user: ${freecodecamp.username}`
+                : `Missing FreeCodeCamp profile`}
+            </p>
+          </>
         ) : progress === 'error' ? (
-          <p>Something went wrong</p>
+          <form onSubmit={handleSubmit}>
+            <p>We couldn't find {websiteUrl || 'your site'}.</p>
+            <p>
+              {websiteUrl
+                ? 'Please check your Github Pages URL is correct.'
+                : 'Please enter your Github Pages URL.'}
+            </p>
+            <label htmlFor="website">Website</label>
+            <input
+              id="website"
+              defaultValue={websiteUrl}
+              placeholder="https://username.github.io/..."
+            />
+            <button type="submit">Try again</button>
+          </form>
         ) : (
           <p>Scanning {websiteUrl}...</p>
         )}
       </section>
       <section>
         <h2>2. Codewars</h2>
-        <Codewars username={data.codewars} />
+        <Codewars username={codewars} />
       </section>
       <section>
         <h2>3. Free Code Camp</h2>
-        <FreeCodeCamp username={data.freecodecamp} />
+        <FreeCodeCamp username={freecodecamp} />
       </section>
     </>
   );
